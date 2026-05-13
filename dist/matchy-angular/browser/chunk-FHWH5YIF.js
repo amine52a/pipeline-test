75ef1023ba3be559c1f8d1a2a153dde771321644
Matchy Deploy <deploy@matchy.app>
2026-05-13 18:33:37 +0100

    fix: Add fileReplacements to angular.json so production build uses environment.prod.ts with live Render URL

---
+++ dist/matchy-angular/browser/chunk-FHWH5YIF.js
@@ -0,0 +1,1 @@
+import{k as a}from"./chunk-XFDYAESH.js";import"./chunk-VDHIPP7F.js";import"./chunk-JCJIXIGU.js";import"./chunk-TG35KS7C.js";import"./chunk-ODN5LVDJ.js";export{a as FrontofficeModule};
